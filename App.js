6c3af12d46f13e4a832b16297255c03fa0c036c6
Ekant Verma <ekantvermasolanki@gmail.com>
2024-06-13 22:51:10 +0530

    files Updated

--- App.js
+++ App.js
@@ -1,1 +1,19 @@
-console.log("Ekant Verma")
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Header component is a functional component
+const HeaderComponent = function () {
+    return (
+      <div>
+        <h1>Namaste React Functional component</h1>
+        <h2>This is h2 tag</h2>
+      </div>
+    );
+  };
+  
+  // Create root using createRoot
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  
+  // Pass the React element inside root
+  root.render(<HeaderComponent />);
+  
--- dist/index.6bd02f5a.js
+++
@@ -1,3 +0,0 @@
-console.log("Ekant Verma");
-
-//# sourceMappingURL=index.6bd02f5a.js.map
